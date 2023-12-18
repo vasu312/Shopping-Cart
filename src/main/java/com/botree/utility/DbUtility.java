c2b399387429f3c363dd3c3e570f90f5fb355251
Vasudevan <87578010+vasu312@users.noreply.github.com>
2023-12-18 12:54:42 +0530

    Update DbUtility.java

--- src/main/java/com/botree/utility/DbUtility.java
+++ src/main/java/com/botree/utility/DbUtility.java
@@ -8,8 +8,8 @@ public class DbUtility {
 	public static Connection getConnection() {
 
 		String url = "jdbc:mysql://localhost:3306/shopping";
-		String username = "botree";
-		String password = "Bsipl@123";
+		String username = "DB_USERNAME";
+		String password = "DB_PASSWORD";
 		Connection connection = null;
 		try {
 
